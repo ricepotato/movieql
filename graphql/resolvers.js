175de64f759d061601622604e028b075661a26d3
Sukjun Sagong <ricepotato40@gmail.com>
2019-12-05 19:00:19 +0900

    people, person schema

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,12 +1,31 @@
-const ricepotato = {
-    name: "ricepotato",
-    age: 35,
-    gender: "female"
-}
+
+const people = [
+    {
+        name: "ricepotato",
+        age: 35,
+        gender: "male"
+    },
+    {
+        name: "h.e.",
+        age: 22,
+        gender: "female"
+    },
+    {
+        name: "g.h.",
+        age: 28,
+        gender: "female"
+    },
+    {
+        name: "yj",
+        age: 29,
+        gender: "female"
+    }
+]
 
 const resolvers = {
     Query: {
-        person: () => ricepotato
+        people: () => people,
+        person: (id) => people[id]
     }
 };
 
